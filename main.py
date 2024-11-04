b18af9580ad169208e10cdac8d4fa39202672e69
Eric M Nunes Campelo <71527280+EricM1303@users.noreply.github.com>
2024-11-04 17:53:33 -0300

    :sparkles: feat: Adição de descrição ao código

---
+++ main.py
@@ -0,0 +1,7 @@
+'''
+Autor: Eric Matheus N Campelo
+Data de criação: 04/11/2024
+Descrição: Calculadora simples, ela faz operações com soma, subtração, multiplicação e divisão. É necessário apenas dar um "RUN" para dar ínicio a ela.
+
+'''
+
